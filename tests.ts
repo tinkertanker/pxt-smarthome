f286a39c0022f8e2f66fdb74ebb77e0ded2c6231
ELECFREAKS <services@elecfreaks.com>
2019-08-07 16:58:44 +0800

    Add files via upload

--- tests.ts
+++ tests.ts
@@ -1,1 +1,7 @@
-tests
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    basic.showNumber(smarthome.ReadSoilHumidity(AnalogPin.P1))
+    basic.showNumber(smarthome.ReadLightIntensity(AnalogPin.P2))
+    basic.showNumber(smarthome.ReadTemperature(TMP36Type.TMP36_temperature_C, AnalogPin.P4))
+    basic.showNumber(smarthome.ReadNoise(AnalogPin.P10))
+})
