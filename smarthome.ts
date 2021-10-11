845bd0b05270cf5a12f9ae85bacf9d63198d801e
elecfreaks1 <support@elecfreaks.com>
2021-10-11 11:08:39 +0800

    Add ultraviolet sensor

--- smarthome.ts
+++ smarthome.ts
@@ -387,4 +387,20 @@ namespace smarthome {
             brightness = 0
         }
     }
+
+    //% blockId="readUVLevel" block="UV sensor %pin level(0~15)"
+    export function UVLevel(pin: AnalogPin): number {
+        let UVlevel = pins.analogReadPin(pin);
+        if (UVlevel > 625) {
+            UVlevel = 625
+        }
+        UVlevel = pins.map(
+            UVlevel,
+            0,
+            625,
+            0,
+            15
+        );
+        return Math.round(UVlevel)
+    }
 }
